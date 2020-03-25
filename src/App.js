2d8490ae8812400ecd8df5927e81740f557a18a9
Jideofor <njusticej@gmail.com>
2020-03-25 15:56:33 +0100

    feat: add dynamically populated table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {getAllStudents} from './client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Table, Avatar, Spin} from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import Container from './container';
+
+const getIndicatorIcon = () => <LoadingOutlined  style={{ fontSize: 24 }} spin />;
+class App extends Component {
+  state = {
+    students: [],
+    isFetching: false
+  }
+
+  componentDidMount(){
+    this.fetchStudents();
+  }
+  fetchStudents = () => {
+    this.setState({
+      isFetching: true
+    });
+    getAllStudents()
+    .then(res => res.json()
+    .then(students => {
+      this.setState({
+        students,
+        isFetching: false
+      });
+    }));
+  }
+  
+  render(){
+    
+    const {students, isFetching} = this.state;
+    if(isFetching){
+      return(
+      <Container>
+         <Spin size ='large' indicator={getIndicatorIcon()} />
+      </Container>
+      )
+    }
+
+    if(students && students.length){
+      const columns = [
+        {
+          title: '',
+          dataIndex: 'avatar',
+          render: (text, student) => (
+            <Avatar size = 'large'>
+              {`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+          )
+        },
+        {
+          title: 'StudentId',
+          dataIndex: 'studentId',
+          key: 'studentId',
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName',
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'lastName',
+          key: 'lastName',
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email',
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender',
+        }
+      ];
+      return (
+        <Container>
+      <Table 
+      dataSource={students} columns={columns} pagination = {false} rowKey ='studentId'
+      />
+      </Container>
+      );
+    }
+    return (
+      <h1>No Students Found</h1>
+        );
+  }
+ 
 }
 
 export default App;
