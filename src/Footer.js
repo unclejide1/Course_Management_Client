d60fee0d56a906978a78dbf0b34795d263c15cdf
Jideofor <njusticej@gmail.com>
2020-03-27 03:22:06 +0100

    feat: handle clientside exceptions

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import Container from './container';
-import { Button, Avatar } from 'antd';
-import './Footer.css';
+import React from "react";
+import Container from "./container";
+import { Button, Avatar } from "antd";
+import "./Footer.css";
 
-const Footer = (props) =>(
-    <div className = 'footer'>
-        <Container>
-                {props.numberOfStudents ? 
-                <Avatar 
-                    style ={{backgroundColor: '#f56a00', marginRight: '5px'}} 
-                    size = 'large'>
-                    {props.numberOfStudents}
-                </Avatar>: null}
-            <Button onClick = {() => props.handleAddStudentClickEvent()} type = 'primary'>
-                 Add new Student +
-            </Button>
-        </Container>
-    </div>
+const Footer = props => (
+  <div className="footer">
+    <Container>
+      {props.numberOfStudents !== undefined ? (
+        <Avatar
+          style={{ backgroundColor: "#f56a00", marginRight: "5px" }}
+          size="large"
+        >
+          {props.numberOfStudents}
+        </Avatar>
+      ) : null}
+      <Button onClick={() => props.handleAddStudentClickEvent()} type="primary">
+        Add new Student +
+      </Button>
+    </Container>
+  </div>
 );
 
 export default Footer;
